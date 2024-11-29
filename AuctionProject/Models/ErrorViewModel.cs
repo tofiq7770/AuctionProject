76d68e99691674f76624d1c00062e262fbb9a6fc
tofiq7770 <tofigtn@code.edu.az>
2024-11-30 02:42:57 +0400

    add

---
+++ AuctionProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuctionProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
