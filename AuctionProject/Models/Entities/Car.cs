91a5853694be3e248ad493e9c9829e7614dc2f58
Tofiq Nasibli <101042076+tofiq7770@users.noreply.github.com>
2024-12-02 01:52:02 +0400

    Update Car.cs

--- AuctionProject/Models/Entities/Car.cs
+++ AuctionProject/Models/Entities/Car.cs
@@ -30,3 +30,33 @@ namespace AuctionProject.Models.Entities
 
     }
 }
+/*
+using AuctionProject.Models.Common;
+
+namespace AuctionProject.Models.Entities
+{
+    public class Car : BaseEntity
+    {
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+        public string Condition { get; set; }
+        public decimal StartingBid { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+
+        // Auction-related fields moved to Auction model
+        public decimal ReservePrice { get; set; }
+
+        // Foreign Keys for related entities
+        public int ModelId { get; set; }
+        public Model Model { get; set; }
+
+        public int ColorID { get; set; }
+        public Color Color { get; set; }
+
+        // Navigation properties
+        public List<Bid> Bids { get; set; } = new List<Bid>();
+    }
+}
+
+*/
