27976f552d45c19dd3cc1fdc0c443483ec23b7c8
Tofiq Nasibli <101042076+tofiq7770@users.noreply.github.com>
2024-12-02 01:51:05 +0400

    Update Auction.cs

--- AuctionProject/Models/Entities/Auction.cs
+++ AuctionProject/Models/Entities/Auction.cs
@@ -16,3 +16,23 @@ namespace AuctionProject.Models.Entities
         public List<Bid> Bids { get; set; } = new List<Bid>();
     }
 }
+
+
+/*using AuctionProject.Models.Common;
+
+namespace AuctionProject.Models.Entities
+{
+    public class Auction : BaseEntity
+    {
+        public int CarID { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public decimal StartingPrice { get; set; }
+        public string Status { get; set; }  // E.g., "Active" or "Closed"
+
+        // Navigation properties
+        public Car Car { get; set; }
+        public List<Bid> Bids { get; set; } = new List<Bid>();
+    }
+}
+*/
