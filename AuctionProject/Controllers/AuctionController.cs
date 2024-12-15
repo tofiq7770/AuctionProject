ee305a67e1cc318f023cecb0aeb1112f0aafea60
tofiq7770 <tofigtn@code.edu.az>
2024-12-16 01:12:41 +0400

    added Front

---
+++ AuctionProject/Controllers/AuctionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionProject.Controllers
+{
+    public class AuctionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
